09dc4455d9e4fad9fd5516b9f6e8799183c6b8e0
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-27 23:28:32 +0530

    Update battery_check_tests.c

--- battery_check_tests.c
+++ battery_check_tests.c
@@ -7,10 +7,16 @@ int main() {
     // Test with English messages
     assert(batteryIsOk(25, 70, 0.7, LANGUAGE_ENGLISH));
     assert(!batteryIsOk(50, 85, 0, LANGUAGE_ENGLISH));
+    assert(!batteryIsOk(30, 85, 0, LANGUAGE_ENGLISH));
+    assert(!batteryIsOk(25, 70, 0.9, LANGUAGE_ENGLISH));
 
     // Test with German messages
     assert(batteryIsOk(25, 70, 0.7, LANGUAGE_GERMAN));
     assert(!batteryIsOk(50, 85, 0, LANGUAGE_GERMAN));
+    assert(!batteryIsOk(30, 85, 0, LANGUAGE_GERMAN));
+    assert(!batteryIsOk(25, 70, 0.9, LANGUAGE_GERMAN));
+    
+    
 
     return 0;
 }
