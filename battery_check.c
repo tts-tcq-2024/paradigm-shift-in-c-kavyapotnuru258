b9c5e2fbaad94d4425eb2387347227eed5789b16
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-27 23:22:35 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -9,7 +9,7 @@ int isTemperatureOk(float temperature, int lang) {
     return 1; // Okay
 }
 
-int isSocOk(int soc, int lang) {
+int isSocOk(float soc, int lang) {
     if (soc < 20 || soc > 80) {
         printf("%s\n", getSocOutOfRangeMessage(lang));
         return 0; // Not okay
@@ -26,7 +26,7 @@ int isChargeRateOk(float chargeRate, int lang) {
 }
 
 // Main battery check function
-int batteryIsOk(float temperature, int soc, float chargeRate, int lang) {
+int batteryIsOk(float temperature, float soc, float chargeRate, int lang) {
     return isTemperatureOk(temperature, lang) &&
            isSocOk(soc, lang) &&
            isChargeRateOk(chargeRate, lang);
