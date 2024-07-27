eb5406b4bacc1771e67efb64c34e207d82f311f4
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-27 22:58:23 +0530

    Update battery_checks.h

--- battery_checks.h
+++ battery_checks.h
@@ -1,21 +1,10 @@
-// battery_checks.h
+#ifndef BATTERY_CHECK_H
+#define BATTERY_CHECK_H
 
-#ifndef BATTERY_CHECKS_H
-#define BATTERY_CHECKS_H
+// Function prototypes
+int isTemperatureOutOfRange(float temperature);
+int isSocOutOfRange(float soc);
+int isChargeRateOutOfRange(float chargeRate);
+int batteryIsOk(float temperature, float soc, float chargeRate, int lang);
 
-// Function prototypes for various battery checks
-int checkTemperature(float temperature);
-int checkSoc(float soc);
-int checkChargeRate(float chargeRate);
-
-// Define Check structure for battery validation
-typedef struct {
-    int (*validate)(float);  // Function pointer for validation
-    float value;             // Value to be validated
-    int messageIndex;        // Index for localized message
-} BatteryCheck;
-
-// Function to validate battery status
-int validateBatteryStatus(float temperature, float soc, float chargeRate);
-
-#endif // BATTERY_CHECKS_H
+#endif // BATTERY_CHECK_H
