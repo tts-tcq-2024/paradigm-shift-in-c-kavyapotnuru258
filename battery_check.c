345fc8a44d6f5d283c79db524ee2034a93f9ed72
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-07-27 23:15:30 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -1,34 +1,20 @@
 #include "battery_check.h"
 #include "language_support.h"
 
-// Check if temperature is out of range
-int isTemperatureOutOfRange(float temperature) {
-    return (temperature < 0 || temperature > 45);
+int isTemperatureOk(float temperature) {
+  return (temperature >= 0 && temperature <= 45) || (printf("Temperature out of range!\n"), 0);
 }
-
-// Check if state of charge is out of range
-int isSocOutOfRange(float soc) {
-    return (soc < 20 || soc > 80);
+ 
+int isSocOk(int soc) {
+  return (soc >= 20 && soc <= 80) || (printf("State of Charge out of range!\n"), 0);
 }
-
-// Check if charge rate is out of range
-int isChargeRateOutOfRange(float chargeRate) {
-    return (chargeRate > 0.8);
+ 
+int isChargeRateOk(float chargeRate) {
+  return (chargeRate <= 0.8) || (printf("Charge Rate out of range!\n"), 0);
 }
-
-// Check if battery is okay and print messages in selected language
-int batteryIsOk(float temperature, float soc, float chargeRate, int lang) {
-    if (isTemperatureOutOfRange(temperature)) {
-        printf("%s\n", getTemperatureOutOfRangeMessage(lang));
-        return 0;
-    }
-    if (isSocOutOfRange(soc)) {
-        printf("%s\n", getSocOutOfRangeMessage(lang));
-        return 0;
-    }
-    if (isChargeRateOutOfRange(chargeRate)) {
-        printf("%s\n", getChargeRateOutOfRangeMessage(lang));
-        return 0;
-    }
-    return 1;
+ 
+int batteryIsOk(float temperature, int soc, float chargeRate) {
+  return isTemperatureOk(temperature) &&
+         isSocOk(soc) &&
+         isChargeRateOk(chargeRate);
 }
