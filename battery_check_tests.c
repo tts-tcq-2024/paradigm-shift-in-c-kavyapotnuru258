9ca419de39be9e631054d342073eadbdfeb70695
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-08-05 21:31:28 +0530

    Update battery_check_tests.c

--- battery_check_tests.c
+++ battery_check_tests.c
@@ -5,16 +5,16 @@
 
 int main() {
     // Test with English messages
-    assert(batteryIsOk(25, 70, 0.7, LANGUAGE_ENGLISH));
-    assert(!batteryIsOk(50, 85, 0, LANGUAGE_ENGLISH));
-    assert(!batteryIsOk(30, 85, 0, LANGUAGE_ENGLISH));
-    assert(!batteryIsOk(25, 70, 0.9, LANGUAGE_ENGLISH));
+    assert(batteryIsOk(25, 70, 0.7, ENGLISH));
+    assert(!batteryIsOk(50, 85, 0, ENGLISH));
+    assert(!batteryIsOk(30, 85, 0, ENGLISH));
+    assert(!batteryIsOk(25, 70, 0.9, ENGLISH));
 
     // Test with German messages
-    assert(batteryIsOk(25, 70, 0.7, LANGUAGE_GERMAN));
-    assert(!batteryIsOk(50, 85, 0, LANGUAGE_GERMAN));
-    assert(!batteryIsOk(30, 85, 0, LANGUAGE_GERMAN));
-    assert(!batteryIsOk(25, 70, 0.9, LANGUAGE_GERMAN));
+    assert(batteryIsOk(25, 70, 0.7, GERMAN));
+    assert(!batteryIsOk(50, 85, 0, GERMAN));
+    assert(!batteryIsOk(30, 85, 0, GERMAN));
+    assert(!batteryIsOk(25, 70, 0.9, GERMAN));
     
     
 
