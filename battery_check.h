c304ec3e6a4a7aca2d3c3b83f530d9d28108af92
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-08-05 21:47:19 +0530

    Update battery_check.h

--- battery_check.h
+++ battery_check.h
@@ -1,10 +1,20 @@
+// #ifndef BATTERY_CHECK_H
+// #define BATTERY_CHECK_H
+
+// // Function prototypes
+// int isTemperatureOutOfRange(float temperature);
+// int isSocOutOfRange(float soc);
+// int isChargeRateOutOfRange(float chargeRate);
+// int batteryIsOk(float temperature, float soc, float chargeRate, int lang);
+
+// #endif // BATTERY_CHECK_H
 #ifndef BATTERY_CHECK_H
 #define BATTERY_CHECK_H
 
 // Function prototypes
-int isTemperatureOutOfRange(float temperature);
-int isSocOutOfRange(float soc);
-int isChargeRateOutOfRange(float chargeRate);
-int batteryIsOk(float temperature, float soc, float chargeRate, int lang);
+int isTemperatureOk(float temperature);
+int isSocOk(float soc);
+int isChargeRateOk(float chargeRate);
+int batteryIsOk(float temperature, float soc, float chargeRate);
 
 #endif // BATTERY_CHECK_H
