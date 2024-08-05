82e4af2139be5e55fd8b0a24c4cabe88cea36f12
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-08-05 21:52:13 +0530

    Update language_support.h

--- language_support.h
+++ language_support.h
@@ -1,18 +1,38 @@
+// #ifndef LANGUAGE_H
+// #define LANGUAGE_H
+// #include <stdio.h>
+
+// // Enum for supported languages
+// typedef enum {
+//             ENGLISH,
+//             GERMAN,
+//     // Add more languages here
+//              COUNT // This should always be the last item
+// } Language;
+
+// // Function prototypes
+// const char* getTemperatureOutOfRangeMessage(Language lang);
+// const char* getSocOutOfRangeMessage(Language lang);
+// const char* getChargeRateOutOfRangeMessage(Language lang);
+
+// #endif // LANGUAGE_H
 #ifndef LANGUAGE_H
 #define LANGUAGE_H
 #include <stdio.h>
 
 // Enum for supported languages
 typedef enum {
-            ENGLISH,
-            GERMAN,
+    ENGLISH,
+    GERMAN,
     // Add more languages here
-             COUNT // This should always be the last item
+    COUNT // This should always be the last item
 } Language;
 
 // Function prototypes
-const char* getTemperatureOutOfRangeMessage(Language lang);
-const char* getSocOutOfRangeMessage(Language lang);
-const char* getChargeRateOutOfRangeMessage(Language lang);
+void setPreferredLanguage(Language lang);
+const char* getTemperatureOutOfRangeMessage();
+const char* getSocOutOfRangeMessage();
+const char* getChargeRateOutOfRangeMessage();
 
 #endif // LANGUAGE_H
+
