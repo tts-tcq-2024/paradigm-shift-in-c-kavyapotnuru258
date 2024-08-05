ef170f97f0bfb6780e21af4ded493e6555907870
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-08-05 21:55:09 +0530

    Update battery_check_tests.c

--- battery_check_tests.c
+++ battery_check_tests.c
@@ -20,23 +20,45 @@
 
 //     return 0;
 // }
+// #include <stdio.h>
+// #include <assert.h>
+// #include "battery_check.h"
+// #include "language_support.h"
+
+// extern Language currentLanguage;
+
+// int main() {
+//     // Test with English messages
+//     currentLanguage = ENGLISH;
+//     assert(batteryIsOk(25, 70, 0.7));
+//     assert(!batteryIsOk(50, 85, 0));
+//     assert(!batteryIsOk(30, 85, 0));
+//     assert(!batteryIsOk(25, 70, 0.9));
+
+//     // Test with German messages
+//     currentLanguage = GERMAN;
+//     assert(batteryIsOk(25, 70, 0.7));
+//     assert(!batteryIsOk(50, 85, 0));
+//     assert(!batteryIsOk(30, 85, 0));
+//     assert(!batteryIsOk(25, 70, 0.9));
+
+//     return 0;
+// }
 #include <stdio.h>
 #include <assert.h>
 #include "battery_check.h"
 #include "language_support.h"
 
-extern Language currentLanguage;
-
 int main() {
-    // Test with English messages
-    currentLanguage = ENGLISH;
+    // Set and test with English messages
+    setPreferredLanguage(ENGLISH);
     assert(batteryIsOk(25, 70, 0.7));
     assert(!batteryIsOk(50, 85, 0));
     assert(!batteryIsOk(30, 85, 0));
     assert(!batteryIsOk(25, 70, 0.9));
 
-    // Test with German messages
-    currentLanguage = GERMAN;
+    // Set and test with German messages
+    setPreferredLanguage(GERMAN);
     assert(batteryIsOk(25, 70, 0.7));
     assert(!batteryIsOk(50, 85, 0));
     assert(!batteryIsOk(30, 85, 0));
