03662fe0acdff3b3f01c9dc4f3d3b57020d28ec9
kavyapotnuru258 <kavyapotnuru258@gmail.com>
2024-08-05 21:52:46 +0530

    Update language_support.c

--- language_support.c
+++ language_support.c
@@ -1,4 +1,37 @@
+// #include "language_support.h"
+
+// // Message arrays for each language
+// const char* messages[COUNT][3] = {
+//     [ENGLISH] = {
+//         "Temperature out of range!",
+//         "State of Charge out of range!",
+//         "Charge Rate out of range!"
+//     },
+//     [GERMAN] = {
+//         "Temperatur außerhalb des Bereichs!",
+//         "Ladezustand außerhalb des Bereichs!",
+//         "Laderate außerhalb des zulässigen Bereichs!"
+//     }
+//     // Add more languages here
+// };
+
+// // Message retrieval functions
+// const char* getTemperatureOutOfRangeMessage(Language lang) {
+//     return messages[lang][0];
+// }
+
+// const char* getSocOutOfRangeMessage(Language lang) {
+//     return messages[lang][1];
+// }
 #include "language_support.h"
+
+// Global variable for storing the preferred language
+static Language currentLanguage = ENGLISH;
+
+// Function to set the preferred language
+void setPreferredLanguage(Language lang) {
+    currentLanguage = lang;
+}
 
 // Message arrays for each language
 const char* messages[COUNT][3] = {
@@ -16,14 +49,19 @@ const char* messages[COUNT][3] = {
 };
 
 // Message retrieval functions
-const char* getTemperatureOutOfRangeMessage(Language lang) {
-    return messages[lang][0];
+const char* getTemperatureOutOfRangeMessage() {
+    return messages[currentLanguage][0];
 }
 
-const char* getSocOutOfRangeMessage(Language lang) {
-    return messages[lang][1];
+const char* getSocOutOfRangeMessage() {
+    return messages[currentLanguage][1];
 }
 
-const char* getChargeRateOutOfRangeMessage(Language lang) {
-    return messages[lang][2];
+const char* getChargeRateOutOfRangeMessage() {
+    return messages[currentLanguage][2];
 }
+
+
+// const char* getChargeRateOutOfRangeMessage(Language lang) {
+//     return messages[lang][2];
+// }
